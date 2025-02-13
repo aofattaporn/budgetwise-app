b7fb7b5ee0d54fa7f960c955c92c9c438492e0db
aofattaporn <aofattapon321@gmail.com>
2025-02-13 22:29:53 +0700

    clean: initial projct folder

---
+++ lib/src/presentation/constant/size.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/widgets.dart';
+
+class SizeConstants {
+  static const kEmptyWidget = SizedBox.shrink();
+
+  static const kPadd2 = EdgeInsets.all(2);
+  static const kPadd5 = EdgeInsets.all(5);
+  static const kPadd6 = EdgeInsets.all(6);
+  static const kPadd10 = EdgeInsets.all(10);
+  static const kPadd15 = EdgeInsets.all(15);
+  static const kPadd20 = EdgeInsets.all(20);
+  static const kPadd25 = EdgeInsets.all(25);
+  static const kPadd30 = EdgeInsets.all(30);
+  static const kPadd40 = EdgeInsets.all(40);
+  static const kPadd50 = EdgeInsets.all(50);
+
+  static const kPaddH6 = EdgeInsets.symmetric(horizontal: 6);
+  static const kPaddH10 = EdgeInsets.symmetric(horizontal: 10);
+  static const kPaddH20 = EdgeInsets.symmetric(horizontal: 20);
+  static const kPaddH25 = EdgeInsets.symmetric(horizontal: 25);
+  static const kPaddH30 = EdgeInsets.symmetric(horizontal: 30);
+  static const kPaddH40 = EdgeInsets.symmetric(horizontal: 40);
+  static const kPaddH50 = EdgeInsets.symmetric(horizontal: 50);
+  static const kPaddH60 = EdgeInsets.symmetric(horizontal: 60);
+
+  static final kRadius0 = BorderRadius.circular(0);
+  static final kRadius10 = BorderRadius.circular(10);
+  static final kRadius15 = BorderRadius.circular(15);
+  static final kRadius30 = BorderRadius.circular(30);
+
+  static const kSizeH45 = Size(0, 45);
+  static const kSizeH50 = Size(0, 50);
+  static const kSizeH60 = Size(0, 60);
+}
--- lib/src/presentation/screens/plan_screen/plan_screen.dart
+++ lib/src/presentation/screens/plan_screen/plan_screen.dart
@@ -1,72 +1,10 @@
-import 'package:budget_wise/src/bloc/plans/plans_bloc.dart';
-import 'package:budget_wise/src/bloc/plans/plans_event.dart';
-import 'package:budget_wise/src/bloc/plans/plans_state.dart';
-import 'package:budget_wise/src/bloc/plan_month_picker/plan_month_picker_bloc.dart';
-import 'package:budget_wise/src/bloc/plan_month_picker/plan_month_picker_event.dart';
-import 'package:budget_wise/src/bloc/usersFin/usersfin_evenet.dart';
-import 'package:budget_wise/src/bloc/usersFin/usersfin_state.dart';
-import 'package:budget_wise/src/bloc/usersFin/usersfin_bloc.dart';
-import 'package:budget_wise/src/presentation/constant/textstyle.dart';
-import 'package:budget_wise/src/presentation/screens/plan_screen/%5Bsheet%5D_create_plan_sheet/create_plan_sheet.dart';
-import 'package:budget_wise/src/presentation/screens/plan_screen/budget_monthyear/budget_monthyear_not_found.dart';
-import 'package:budget_wise/src/presentation/screens/plan_screen/budget_monthyear/budget_monthyear_success.dart';
-import 'package:budget_wise/src/presentation/screens/plan_screen/budget_monthyear/budget_monthyear_failure.dart';
-import 'package:budget_wise/src/presentation/screens/plan_screen/budget_monthyear/budget_monthyear_loading.dart';
-import 'package:budget_wise/src/presentation/screens/plan_screen/display_plans/display_plans_faillure.dart';
-import 'package:budget_wise/src/presentation/screens/plan_screen/display_plans/display_plans_loading.dart';
-import 'package:budget_wise/src/presentation/screens/plan_screen/display_plans/display_plans_success.dart';
-import 'package:budget_wise/src/presentation/screens/plan_screen/month_year_picker/month_year_picker.dart';
-import 'package:budget_wise/src/presentation/theme/color_scheme.dart';
-import 'package:budget_wise/src/presentation/theme/padding_scheme.dart';
-import 'package:budget_wise/src/presentation/ui/generic_txt_btn.dart';
-import 'package:budget_wise/src/presentation/widgets/progress_bar/progress_bar.dart';
-import 'package:budget_wise/src/utils/datetime_util.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class PlanScreen extends StatelessWidget {
   const PlanScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final MonthPickerBloc monthPickerBloc = MonthPickerBloc();
-    final UsersFinBloc usersFinBloc = UsersFinBloc();
-
-    String initialMonthYear = UtilsDateTime.yearMonthFormat(DateTime.now());
-
-    // Initialize default state
-    context.read<PlansBloc>().add(GetPlansEvent(monthYear: initialMonthYear));
-    monthPickerBloc.add(SetMonthPickerEvent(monthYear: DateTime.now()));
-
-    return SingleChildScrollView(
-      clipBehavior: Clip.none,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            width: MediaQuery.of(context).size.width,
-            child: const Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  "June 2024",
-                  style: TextStyle(
-                      fontSize: 16, color: AppColorScheme.onBackground),
-                ),
-                Text(
-                  "3000 B",
-                  style: TextStyle(
-                      fontSize: 32,
-                      color: AppColorScheme.onBackground,
-                      fontWeight: FontWeight.bold),
-                ),
-                ProgressBar(isFullSize: false, progress: 0)
-              ],
-            ),
-          )
-        ],
-      ),
-    );
+    return const SingleChildScrollView();
   }
 }
