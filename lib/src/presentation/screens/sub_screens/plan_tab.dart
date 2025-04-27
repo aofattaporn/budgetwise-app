8cbc92f2b4677d9ebe978d25de88b92d9cffbdb4
aofattaporn <aofattapon321@gmail.com>
2025-04-27 10:16:43 +0700

    refactor: planTab

--- lib/src/presentation/screens/sub_screens/plan_tab.dart
+++ lib/src/presentation/screens/sub_screens/plan_tab.dart
@@ -5,7 +5,6 @@ import 'package:budget_wise/src/common/theme/app_shadow.dart';
 import 'package:budget_wise/src/common/theme/app_text_style.dart';
 import 'package:budget_wise/src/core/constant/business_constant.dart';
 import 'package:budget_wise/src/core/utils/numbers_uti.dart';
-import 'package:budget_wise/src/domain/entities/plan_item_entity.dart';
 import 'package:budget_wise/src/presentation/bloc/plan_bloc/plan_bloc.dart';
 import 'package:budget_wise/src/presentation/bloc/plan_bloc/plan_event.dart';
 import 'package:budget_wise/src/presentation/bloc/plan_bloc/plan_state.dart';
@@ -14,17 +13,13 @@ import 'package:budget_wise/src/presentation/bloc/plan_item_bloc/plan_item_bloc.
 import 'package:budget_wise/src/presentation/bloc/plan_item_bloc/plan_item_event.dart';
 import 'package:budget_wise/src/presentation/bloc/plan_item_bloc/plan_item_state.dart';
 import 'package:budget_wise/src/presentation/widgets/amount_compare.dart';
+import 'package:budget_wise/src/presentation/widgets/plan_item.dart';
 import 'package:budget_wise/src/presentation/widgets/segmented_circular_progress.dart';
+import 'package:budget_wise/src/presentation/widgets/summary_plan_segemennt.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:skeletonizer/skeletonizer.dart';
 
-/// The main screen tab for displaying a plan-related view.
-///
-/// This widget displays various components, including:
-/// - A segmented circular progress bar showing the usage of different segments.
-/// - A summary container showing the details of transaction segments like "Transfers", "Savings", etc.
-/// - A placeholder for a list of items related to the plan.
 class PlanTab extends StatefulWidget {
   const PlanTab({super.key});
 
@@ -36,7 +31,7 @@ class _PlanTabState extends State<PlanTab> {
   final List<String> planShowLabels = [
     BusinessConstant.savingType,
     BusinessConstant.tranfersType,
-    BusinessConstant.unplannedType
+    BusinessConstant.unplannedType,
   ];
 
   @override
@@ -45,95 +40,36 @@ class _PlanTabState extends State<PlanTab> {
     context.read<PlanBloc>().add(FetchCurrentMonthPlan());
   }
 
-  void saveNewPlanning(BuildContext context) {}
-
-  // final List<PlanItem> mockPlanItems = [
-  //   PlanItem(name: 'Saving for Car', totalAmount: 10000, usedAmount: 2500),
-  //   PlanItem(name: 'Trip to Japan', totalAmount: 50000, usedAmount: 15000),
-  //   PlanItem(name: 'Emergency Fund', totalAmount: 20000, usedAmount: 5000),
-  //   PlanItem(name: 'Gadget Upgrade', totalAmount: 8000, usedAmount: 2000),
-  // ];
+  void _saveNewPlanning(BuildContext context) {
+    // TODO: implement create plan item
+  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: AppPadding.allxl,
-      // ! [ PlanBloc ] : update curcular progress pocket
       child: BlocBuilder<PlanBloc, PlanState>(
-        builder: (BuildContext context, PlanState planState) {
-          // handle provide plan state
-          final bool isLoading = planState is PlanLoading;
-          final bool isSuccess = planState is PlanLoaded;
-          final bool isNotfound = planState is PlanNotFound;
-
-          if (planState is PlanError) {
+        builder: (context, state) {
+          if (state is PlanError) {
             WidgetsBinding.instance.addPostFrameCallback((_) {
-              CommonNotification.showSnackBar(context, planState.message);
+              CommonNotification.showSnackBar(context, state.message);
             });
           }
+
+          final isLoading = state is PlanLoading;
+          final isSuccess = state is PlanLoaded;
+          final isNotFound = state is PlanNotFound;
 
           return Skeletonizer(
             enabled: isLoading,
             child: Column(
-              spacing: 12,
               children: [
-                Container(
-                  padding: AppPadding.hxxs,
-                  decoration: BoxDecoration(
-                    color: AppColors.transparent,
-                    borderRadius: BorderRadius.circular(16),
-                  ),
-                  child: GestureDetector(
-                    onTap: () => isSuccess
-                        ? CustomCommonSheet()
-                            .allPlansSheet(context, planState.plan.id ?? 0)
-                        : null,
-                    child: Center(
-                      child: MultiSegmentCircularProgress(
-                        isNotfound: isNotfound,
-                        plan: isSuccess ? planState.plan : null,
-                      ),
-                    ),
-                  ),
-                ),
-                isSuccess
-                    ? Column(
-                        spacing: 12,
-                        children: [
-                          SummaryPlanSegment(
-                              limitAmount: planState.plan.totalBudget,
-                              segmentTilel: BusinessConstant.savingType,
-                              usage: planState.plan.summarySaving,
-                              segmentColor: AppColors.priamryDark),
-                          SummaryPlanSegment(
-                              limitAmount: planState.plan.totalBudget,
-                              segmentTilel: BusinessConstant.tranfersType,
-                              usage: planState.plan.summaryTranfer,
-                              segmentColor: AppColors.primary),
-                          SummaryPlanSegment(
-                              limitAmount: planState.plan.totalBudget,
-                              segmentTilel: BusinessConstant.unplannedType,
-                              usage: planState.plan.summaryOther,
-                              segmentColor: AppColors.primarySubtle)
-                        ],
-                      )
-                    : const SizedBox(),
-
-                // ! [ PlanItemBloc ] :
-                isSuccess
-                    ? Expanded(
-                        child: Column(
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [_buildActionPlan(context)],
-                            ),
-                            const SizedBox(height: 16), // add spacing between
-                            _buildAllPlanItem(planState.plan.id!),
-                          ],
-                        ),
-                      )
-                    : const Spacer(),
+                _buildPlanProgress(state, isSuccess, isNotFound),
+                const SizedBox(height: 12),
+                if (isSuccess) _buildPlanSegments(state),
+                const SizedBox(height: 12),
+                if (isSuccess) _buildPlanItems(state),
+                if (!isSuccess) const Spacer(),
               ],
             ),
           );
@@ -142,15 +78,82 @@ class _PlanTabState extends State<PlanTab> {
     );
   }
 
-  Widget _buildActionPlan(BuildContext context) {
+  Widget _buildPlanProgress(PlanState state, bool isSuccess, bool isNotFound) {
+    return Container(
+      padding: AppPadding.hxxs,
+      decoration: BoxDecoration(
+        color: AppColors.transparent,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: GestureDetector(
+        onTap: () {
+          if (isSuccess) {
+            final planId = (state as PlanLoaded).plan.id ?? 0;
+            CustomCommonSheet().allPlansSheet(context, planId);
+          }
+        },
+        child: Center(
+          child: MultiSegmentCircularProgress(
+            isNotfound: isNotFound,
+            plan: isSuccess ? (state as PlanLoaded).plan : null,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPlanSegments(PlanState state) {
+    final plan = (state as PlanLoaded).plan;
+
+    return Column(
+      children: [
+        SummaryPlanSegment(
+          limitAmount: plan.totalBudget,
+          segmentTilel: BusinessConstant.savingType,
+          usage: plan.summarySaving,
+          segmentColor: AppColors.priamryDark,
+        ),
+        const SizedBox(height: 8),
+        SummaryPlanSegment(
+          limitAmount: plan.totalBudget,
+          segmentTilel: BusinessConstant.tranfersType,
+          usage: plan.summaryTranfer,
+          segmentColor: AppColors.primary,
+        ),
+        const SizedBox(height: 8),
+        SummaryPlanSegment(
+          limitAmount: plan.totalBudget,
+          segmentTilel: BusinessConstant.unplannedType,
+          usage: plan.summaryOther,
+          segmentColor: AppColors.primarySubtle,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildPlanItems(PlanState state) {
+    final planId = (state as PlanLoaded).plan.id!;
+    context.read<PlanItemBloc>().add(FetchPlanItemEvent(planId));
+
+    return Expanded(
+      child: Column(
+        children: [
+          _buildActionButton(),
+          const SizedBox(height: 16),
+          _buildPlanItemList(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildActionButton() {
     return Padding(
       padding: AppPadding.hxxs,
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.end,
         children: [
           TextButton.icon(
-            onPressed: () => saveNewPlanning(context),
+            onPressed: () => _saveNewPlanning(context),
             icon: const Icon(Icons.add, color: Colors.white),
             label: const Text(
               "New Item",
@@ -169,13 +172,9 @@ class _PlanTabState extends State<PlanTab> {
     );
   }
 
-  Widget _buildAllPlanItem(int planId) {
-    // 🚨 อย่า .add() ใน build นะ เดี๋ยวอธิบายต่อท้าย
-    context.read<PlanItemBloc>().add(FetchPlanItemEvent(planId));
-
+  Widget _buildPlanItemList() {
     return Expanded(
       child: Container(
-        margin: const EdgeInsets.only(top: 0),
         decoration: BoxDecoration(
           color: AppColors.white,
           borderRadius: BorderRadius.circular(12),
@@ -184,46 +183,11 @@ class _PlanTabState extends State<PlanTab> {
         child: BlocBuilder<PlanItemBloc, PlanItemState>(
           builder: (context, state) {
             if (state is PlanItemLoading) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
+              return const Center(child: CircularProgressIndicator());
             }
 
             if (state is PlanItemEmpty) {
-              return const Center(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 24),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.inbox,
-                        size: 56,
-                        color: AppColors.primarySubtle,
-                      ),
-                      SizedBox(height: 24),
-                      Text(
-                        "No Items Yet",
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          color: AppColors.priamryDark,
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                      SizedBox(height: 8),
-                      Text(
-                        "Create your first item.",
-                        style: TextStyle(
-                          fontSize: 14,
-                          color: AppColors.primarySubtle,
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                    ],
-                  ),
-                ),
-              );
+              return _buildEmptyPlanItemView();
             }
 
             if (state is PlanItemLoaded) {
@@ -238,121 +202,47 @@ class _PlanTabState extends State<PlanTab> {
               );
             }
 
-            return const Center(
-              child: Text("Something went wrong..."), // fallback case
-            );
+            return const Center(child: Text("Something went wrong..."));
           },
         ),
       ),
     );
   }
-}
 
-class PlannItem extends StatelessWidget {
-  const PlannItem({
-    super.key,
-    required this.item,
-    required this.progress,
-  });
-
-  final PlanItemEntity item;
-  final double progress;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(bottom: 16),
-      padding: const EdgeInsets.all(12),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8),
-        color: AppColors.primarySubtle.withOpacity(0.1),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // Top Row: Name + Amount + Icon
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    item.title,
-                    style: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 4),
-                  Text(
-                    '${item.usage} ฿',
-                    style: const TextStyle(
-                      fontSize: 14,
-                      color: Colors.grey,
-                    ),
-                  ),
-                ],
-              ),
-              IconButton(
-                icon: const Icon(Icons.more_vert),
-                onPressed: () {
-                  // TODO: your action here
-                },
-              ),
-            ],
-          ),
-
-          const SizedBox(height: 12),
-
-          // Progress Bar
-          ClipRRect(
-            borderRadius: BorderRadius.circular(8),
-            child: LinearProgressIndicator(
-              value: progress.clamp(0.0, 1.0),
-              backgroundColor: AppColors.primarySubtle.withOpacity(0.3),
-              valueColor:
-                  const AlwaysStoppedAnimation<Color>(AppColors.primary),
-              minHeight: 8,
+  Widget _buildEmptyPlanItemView() {
+    return const Center(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.inbox,
+              size: 56,
+              color: AppColors.primarySubtle,
             ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class SummaryPlanSegment extends StatelessWidget {
-  final String segmentTilel;
-  final double usage;
-  final double limitAmount;
-  final Color segmentColor;
-  const SummaryPlanSegment({
-    super.key,
-    required this.segmentTilel,
-    required this.usage,
-    required this.limitAmount,
-    required this.segmentColor,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      spacing: 8,
-      children: [
-        Container(
-            padding: const EdgeInsets.all(4),
-            decoration: BoxDecoration(
-              color: segmentColor,
-              borderRadius: const BorderRadius.all(Radius.circular(4)),
+            SizedBox(height: 24),
+            Text(
+              "No Items Yet",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: AppColors.priamryDark,
+              ),
+              textAlign: TextAlign.center,
             ),
-            child: Text(segmentTilel,
-                style: const TextStyle(color: AppColors.white))),
-        Text('(${NumberUtil.calPercentage(usage, limitAmount).toString()}%)',
-            style: AppTextStyles.labelGraySmall),
-        const Spacer(),
-        AmountCompare(usage: usage, limitAmount: limitAmount)
-      ],
+            SizedBox(height: 8),
+            Text(
+              "Create your first item.",
+              style: TextStyle(
+                fontSize: 14,
+                color: AppColors.primarySubtle,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
