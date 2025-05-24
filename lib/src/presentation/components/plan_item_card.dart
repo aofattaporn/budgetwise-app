36efed8b3b05cfc157912af1cbf4ad92286c9450
aofattaporn <aofattapon321@gmail.com>
2025-05-24 13:53:59 +0700

    feat: custom plan_item card

--- lib/src/presentation/components/plan_item_card.dart
+++ lib/src/presentation/components/plan_item_card.dart
@@ -1,9 +1,14 @@
+import 'package:budget_wise/src/domain/models/plan_item_dto.dart';
 import 'package:budget_wise/src/presentation/theme/system/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class PlanItemCard extends StatelessWidget {
+  final PlanItemDto planItemDto;
+  final double planBudgetLiit;
   const PlanItemCard({
     super.key,
+    required this.planItemDto,
+    required this.planBudgetLiit,
   });
 
   @override
@@ -11,88 +16,79 @@ class PlanItemCard extends StatelessWidget {
     final double cardWidth = MediaQuery.of(context).size.height * 0.2;
 
     return Container(
-      width: cardWidth,
-      margin: const EdgeInsets.only(right: 12),
-      decoration: BoxDecoration(
-        color: Theme.of(context).primaryColor,
-        borderRadius: BorderRadius.circular(10),
-        boxShadow: const [
-          BoxShadow(
-            color: AppColors.primaryDark,
-            spreadRadius: 2,
-            blurRadius: 12,
-            offset: Offset(0, 4),
-          ),
-        ],
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          spacing: 8,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              spacing: 8,
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                const Spacer(),
-                Text("26%",
-                    style: Theme.of(context)
-                        .textTheme
-                        .headlineLarge!
-                        .copyWith(color: AppColors.background)),
-              ],
-            ),
-            const Icon(
-              Icons.home_outlined,
-              size: 36,
-              color: AppColors.background,
-            ),
-            Row(
-              spacing: 8,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Text("Eat sdfsdf",
-                    style: Theme.of(context)
-                        .textTheme
-                        .displaySmall!
-                        .copyWith(color: AppColors.background))
-              ],
-            ),
-            Column(
-              children: [
-                _buildPlanItemDetail(context, "on usage", "20,000"),
-                _buildPlanItemDetail(context, "on usage", "20,000"),
-              ],
+        width: cardWidth,
+        margin: const EdgeInsets.only(right: 12),
+        decoration: BoxDecoration(
+          color: Theme.of(context).primaryColor,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: const [
+            BoxShadow(
+              color: AppColors.primaryDark,
+              spreadRadius: 2,
+              blurRadius: 12,
+              offset: Offset(0, 4),
             ),
           ],
         ),
-      ),
-    );
-  }
-
-  Widget _buildPlanItemDetail(
-      BuildContext context, String label, String value) {
-    return Row(
-      spacing: 12,
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.end,
-      children: [
-        Text(
-          label,
-          style: Theme.of(context)
-              .textTheme
-              .bodyMedium!
-              .copyWith(color: AppColors.background),
-        ),
-        Text(
-          value,
-          style: Theme.of(context)
-              .textTheme
-              .bodyMedium!
-              .copyWith(color: AppColors.background),
-        )
-      ],
-    );
+        child: Container(
+          padding: const EdgeInsets.all(12),
+          height: 120,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: AppColors.primary,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    planItemDto.iconName ?? "-",
+                    style: Theme.of(context)
+                        .textTheme
+                        .headlineLarge!
+                        .copyWith(color: AppColors.background),
+                  ),
+                  Text(
+                    "4%",
+                    style: Theme.of(context)
+                        .textTheme
+                        .headlineMedium!
+                        .copyWith(color: AppColors.background),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 4),
+              Text(
+                "Food",
+                style: Theme.of(context)
+                    .textTheme
+                    .labelLarge!
+                    .copyWith(color: AppColors.background),
+              ),
+              const SizedBox(height: 2),
+              Text(
+                "0 / ${planItemDto.amountLimit} THB",
+                style: Theme.of(context)
+                    .textTheme
+                    .labelLarge!
+                    .copyWith(color: AppColors.background),
+              ),
+              const SizedBox(height: 6),
+              const SizedBox(
+                width: double.infinity,
+                height: 6, // optional height
+                child: LinearProgressIndicator(
+                  value: 0.42,
+                  backgroundColor: AppColors.background,
+                  valueColor:
+                      AlwaysStoppedAnimation<Color>(AppColors.primaryDark),
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
--- lib/src/presentation/screens/main/sub_screens/plan_tab.dart
+++ lib/src/presentation/screens/main/sub_screens/plan_tab.dart
@@ -60,7 +60,7 @@ class _PlanTabState extends State<PlanTab> {
           children: [
             Container(
               width: double.infinity,
-              height: screenHeight * 0.8, //  80% of bg
+              height: screenHeight * 0.7, //  80% of bg
               padding: const EdgeInsets.all(16),
               decoration: AppDecorations.gradientBottomRounded,
               child: Column(
@@ -296,36 +296,41 @@ class _PlanTabState extends State<PlanTab> {
           ),
           const SizedBox(height: 12),
           Expanded(
-            child: BlocBuilder<PlanItemBloc, PlanItemState>(
-              builder: (context, state) {
-                if (state is PlanItemLoading) {
-                  return const Center(child: CircularProgressIndicator());
-                }
-
-                if (state is PlanItemError) {
-                  return Center(
-                    child: Text(
-                      "Failed to load items: ${state.message}",
-                      style: Theme.of(context)
-                          .textTheme
-                          .bodyLarge!
-                          .copyWith(color: AppColors.error),
-                    ),
-                  );
-                }
-
-                if (state is PlanItemLoaded) {
-                  final items = state.items;
-                  if (items.isEmpty) {
-                    return _buildEmptyPlanItemMessage(context);
+            child: BlocBuilder<CurrentPlanBloc, CurrentPlanState>(
+                builder: (context, planState) {
+              final isLoaded = (planState is CurrentPlanLoaded);
+              return BlocBuilder<PlanItemBloc, PlanItemState>(
+                builder: (context, state) {
+                  if (state is PlanItemLoading) {
+                    return const Center(child: CircularProgressIndicator());
                   }
 
-                  return _buildPlanItemCarousel(items);
-                }
+                  if (state is PlanItemError) {
+                    return Center(
+                      child: Text(
+                        "Failed to load items: ${state.message}",
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyLarge!
+                            .copyWith(color: AppColors.error),
+                      ),
+                    );
+                  }
 
-                return const SizedBox();
-              },
-            ),
+                  if (state is PlanItemLoaded) {
+                    final items = state.items;
+                    if (items.isEmpty) {
+                      return _buildEmptyPlanItemMessage(context);
+                    }
+
+                    return _buildPlanItemCarousel(
+                        items, isLoaded ? planState.plan.amountLimit : 0);
+                  }
+
+                  return const SizedBox();
+                },
+              );
+            }),
           ),
         ],
       ),
@@ -357,14 +362,18 @@ class _PlanTabState extends State<PlanTab> {
     );
   }
 
-  Widget _buildPlanItemCarousel(List<PlanItemDto> items) {
+  Widget _buildPlanItemCarousel(List<PlanItemDto> items, double amountLimit) {
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
       child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: items
-            .map((item) => const Padding(
-                  padding: EdgeInsets.only(right: 8.0),
-                  child: PlanItemCard(),
+            .map((item) => Padding(
+                  padding: const EdgeInsets.only(right: 8.0),
+                  child: PlanItemCard(
+                    planItemDto: item,
+                    planBudgetLiit: amountLimit,
+                  ),
                 ))
             .toList(),
       ),
