f422d98b0b40af410d9d4ae5f6b1566feb4a611a
aofattaporn <aofattapon321@gmail.com>
2025-05-24 20:42:28 +0700

    fix: unknown page route

--- lib/src/presentation/routes/app_screens.dart
+++ lib/src/presentation/routes/app_screens.dart
@@ -3,7 +3,6 @@ import 'package:budget_wise/src/presentation/common/unknown_screen.dart';
 import 'package:budget_wise/src/presentation/routes/app_routes.dart';
 import 'package:budget_wise/src/presentation/screens/main/main_screen.dart';
 import 'package:budget_wise/src/presentation/screens/plans/plan_detail_screen.dart';
-import 'package:budget_wise/src/presentation/screens/plans/plan_item_overview_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -16,24 +15,10 @@ class AppPages {
         final planDto = settings.arguments as PlanDto?;
         return CupertinoPageRoute(
             builder: (context) => PlanDetailScreen(planDto: planDto));
-      case AppRoutes.planItemOverviewScreen:
-        return CupertinoPageRoute(
-            builder: (context) => const PlanItemOverviewScreen());
       default:
         return MaterialPageRoute(
           builder: (context) => const UnknownScreen(),
         );
     }
   }
-
-  // // (SelectIconScreen)
-  // static CupertinoPageRoute<dynamic> _prepareSelectIconScreen(
-  //     RouteSettings settings) {
-  //   final args = settings.arguments as SelectIconScreen;
-  //   return CupertinoPageRoute(
-  //       builder: (context) => SelectIconScreen(
-  //             initialIcon: args.initialIcon,
-  //             handleSelectIcon: args.handleSelectIcon,
-  //           ));
-  // }
 }
